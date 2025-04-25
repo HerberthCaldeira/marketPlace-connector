29ea200f3adfcffa11085bbedf753bf3960c9c27
herberth <herberthcaldeira@gmail.com>
2025-04-24 22:54:36 -0300

    horazionprovider and test file

--- tests/Feature/ImportOfferTest.php
+++ tests/Feature/ImportOfferTest.php
@@ -2,8 +2,10 @@
 
 declare(strict_types = 1);
 
-use App\Domains\Offers\Jobs\ImportOffersJob;
+use App\Domains\Offers\Contracts\IMarketingPlaceClient;
+use App\Domains\Offers\Jobs\StartImportOffersJob;
 use App\Events\ImportOffersRequestedEvent;
+use App\Infrastructure\Marketplace\MarketPlaceClient;
 use App\Listeners\ImportOffersRequestedListener;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Queue;
@@ -26,5 +28,22 @@ it('should be able to push a job to the queue when import offer is requested', f
 
     $response->assertStatus(200);
 
-    Queue::assertPushed(ImportOffersJob::class);
+    Queue::assertPushed(StartImportOffersJob::class);
 });
+
+it('should be able to get a concret class from a typehint interface', function (): void {
+    $marketingPlaceClient = app(IMarketingPlaceClient::class);
+    expect($marketingPlaceClient)->toBeInstanceOf(MarketPlaceClient::class);
+});
+
+it('should be able to get offers from marketplace', function (): void {
+    $marketingPlaceClient = app(IMarketingPlaceClient::class);
+    $offers               = $marketingPlaceClient->getAllOffersFromAPage(1);
+    ds($offers);
+    expect($offers)->toBeArray();
+});
+
+// it('job', function (): void {
+
+//     new ImportOffersJob()->handle();
+// });
